b3fe5fd4324b217829396e21530277fe9bbc0394
MVe11111 <90624942+Sprudelflasch@users.noreply.github.com>
2023-09-26 15:43:01 +0200

    Changes

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,43 @@
 // Schritt 1: Definition des Studententyps
 type Student = {
-    firstName: string;
-    lastName: string;
-    age: number;
+    firstName: string,
+    lastName: string,
+    age: number,
     grades: (number | string | undefined)[];
 };
 
 // Schritt 2: Funktion zur Ausgabe von Studentendaten
 function printStudent(student: Student) {
+    console.log("============================================================");
+
     const { firstName, lastName, age, grades } = student;
-    console.log(`${firstName} ${lastName} (${age})`);
-    console.log("==============================");
-    console.log("Noten:");
+    console.log(`====> ${firstName} ${lastName} // Age:(${age})`);
+    console.log("----------------------");
+    console.log("====> Noten:");
     console.log(grades.map((grade) => (grade === undefined ? "*" : grade)).join(","));
-    console.log("");
+    console.log("============================================================");
 }
 
 // Schritt 3: Beispiel-Studenten erstellen
-const student1: Student = {
-    firstName: "Anton",
-    lastName: "Meier",
-    age: 17,
-    grades: [1, 4, 3, 1, "A", undefined, 1, 2],
+let student1: Student = {
+    firstName: "Romi",
+    lastName: "Waalkes",
+    age: 26,
+    grades: [1,4,3,1,3,2,1,2],
 };
 
-const student2: Student = {
-    firstName: "Berta",
-    lastName: "Müller",
-    age: 17,
-    grades: ["A", undefined, 1],
+let student2: Student = {
+    firstName: "Bella",
+    lastName: "B",
+    age: 22,
+    grades: ["A",2,"F",3,1,"B",2,5],
 };
 
-const student3: Student = {
-    firstName: "Cäsar",
-    lastName: "Schmidt",
-    age: 17,
-    grades: ["A", 1, undefined, 3, 2, 4, 5],
+let student3: Student = {
+    firstName: "Anna",
+    lastName: "Wiebke",
+    age: 23,
+    grades: ["A", 2,undefined,3,1,"B",undefined,5],
 };
 
 // Schritt 4: Funktion zur Ausgabe einer Liste von Studenten
@@ -46,5 +48,5 @@ function printStudentList(students: Student[]) {
 }
 
 // Beispiel: Liste der Studenten ausgeben
-const studentList: Student[] = [student1, student2, student3];
+let studentList: Student[] = [student1, student2, student3];
 printStudentList(studentList);
